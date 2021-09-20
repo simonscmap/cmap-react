b66092c306b5db5bc554edfddbc9126239a74e2e
Walker Malling <mallinwa@uw.edu>
2021-09-20 09:40:49 -0700

    add redux dev tools
    also add prettier for quick formatting

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,12 +2,15 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./Reducers";
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './sagas';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware)
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+  )
 )
 
 sagaMiddleware.run(rootSaga);
