a1c9fb9a223c631d38cfa8f26232bb17b8e081b5
Walker Malling <mallinwa@uw.edu>
2021-09-21 13:04:50 -0700

    add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// these options configure formatting behavior of prettier.js
+// https://prettier.io/docs/en/configuration.html
+module.exports = {
+  printWidth: 80,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  bracketSpace: true,
+  bracketLine: false,
+  arrowParens: "always",
+};
