f5a1242b4afacbbfba7409a32562253c7147d6d8
Walker Malling <mallinwa@uw.edu>
2021-09-29 11:53:43 -0700

    reformat TopNavBar
    automatic with prettier; separating this from substantive diff

--- src/Components/UI/TopNavBar.js
+++ src/Components/UI/TopNavBar.js
@@ -1,203 +1,284 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from "react-router";
-import { Link } from "react-router-dom";
-
+import { withRouter } from 'react-router';
+import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-
-import { showLoginDialog, restoreInterfaceDefaults, snackbarOpen, toggleShowHelp, setShowCart } from '../../Redux/actions/ui';
+import {
+  showLoginDialog,
+  restoreInterfaceDefaults,
+  snackbarOpen,
+  toggleShowHelp,
+  setShowCart,
+} from '../../Redux/actions/ui';
 import { guestPlotLimitNotificationSetIsVisible } from '../../Redux/actions/visualization';
 import { logOut } from '../../Redux/actions/user';
 import { Typography } from '@material-ui/core';
-
 import UserNavbarDropdown from '../User/UserNavbarDropdown';
 import DataSubmissionNavbarDropdown from '../DataSubmission/DataSubmissionNavbarDropdown';
 import MobileNavbarMenu from './MobileNavbarMenu';
-
 import JSS from '../../Stylesheets/JSS';
 import VizNavbarDropdown from '../Visualization/VizNavbarDropdown';
-
 import z from '../../enums/zIndex';
 
-const styles = theme => ({
-
-    simonsLogo: {        
-        verticalAlign: 'middle',
-        display: 'inline-block',
-    },
-    
-    simonsLogoWrapper: {
-        pointerEvents: 'all',
-        display: 'inline-block',
-        marginRight: '40px !important',     
-        verticalAlign: 'middle',   
-    },
-
-    navWrapper: {
-        position: 'fixed',
-        width: '100vw',
-        top: '0px',
-        backgroundColor: 'transparent',
-        zIndex: z.NAVBAR,
-        paddingTop: '14px',
-        paddingBottom: '14px',
-        paddingLeft: '20px',
-        paddingRight: '20px',
-        textAlign: 'left',
-        boxSizing: 'border-box',
-        display: 'flex',
-        justifyContent: 'space-between',
-        pointerEvents: 'none'
-    },
-
-    navWrapperBlue: {
-        backgroundColor: '#1D4962'
-    },
-
-    rightSectionWrapper: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-    },
-
-    navLink: JSS.navLink(theme),
-
-    rightNavLink: {
-        marginRight: 24
-    },    
-})
+const styles = (theme) => ({
+  simonsLogo: {
+    verticalAlign: 'middle',
+    display: 'inline-block',
+  },
+  simonsLogoWrapper: {
+    pointerEvents: 'all',
+    display: 'inline-block',
+    marginRight: '40px !important',
+    verticalAlign: 'middle',
+  },
+  navWrapper: {
+    position: 'fixed',
+    width: '100vw',
+    top: '0px',
+    backgroundColor: 'transparent',
+    zIndex: z.NAVBAR,
+    paddingTop: '14px',
+    paddingBottom: '14px',
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    textAlign: 'left',
+    boxSizing: 'border-box',
+    display: 'flex',
+    justifyContent: 'space-between',
+    pointerEvents: 'none',
+  },
+  navWrapperBlue: {
+    backgroundColor: '#1D4962',
+  },
+  rightSectionWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  navLink: JSS.navLink(theme),
+  rightNavLink: {
+    marginRight: 24,
+  },
+});
 
 const mapStateToProps = (state, ownProps) => ({
-    user: state.user,
-    showHelp: state.showHelp,
-    cart: state.cart,
-    userIsGuest: state.userIsGuest
-})
+  user: state.user,
+  showHelp: state.showHelp,
+  cart: state.cart,
+  userIsGuest: state.userIsGuest,
+});
 
 const mapDispatchToProps = {
-    showLoginDialog,
-    logOut,
-    restoreInterfaceDefaults,
-    snackbarOpen,
-    toggleShowHelp,
-    setShowCart,
-    guestPlotLimitNotificationSetIsVisible
-}
+  showLoginDialog,
+  logOut,
+  restoreInterfaceDefaults,
+  snackbarOpen,
+  toggleShowHelp,
+  setShowCart,
+  guestPlotLimitNotificationSetIsVisible,
+};
 
 const breakpoint = 940;
 
 class TopNavBar extends Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            layout: window.innerWidth >= breakpoint ? 'desktop' : 'mobile'
-        }
-        
-    }
-    
-    componentDidMount = () => {
-        window.addEventListener("resize", this.handleResize);
-    }
-
-    handleResize = (e) => {
-        let w = e.target.innerWidth;
-
-        if(w < breakpoint && this.state.layout === 'desktop'){
-            this.setState({...this.state, layout:'mobile'});
-        }
-
-        else if(w >= breakpoint && this.state.layout === 'mobile'){
-            this.setState({...this.state, layout:'desktop'});
-        }
-    }
-
-    // Controls login dialog text fields to allow reset from logout button
-    handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value})
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      layout: window.innerWidth >= breakpoint ? 'desktop' : 'mobile',
     };
+  }
 
-    handleLogOut = () => {
-        this.clearState();
-        this.props.logOut();
-        this.props.restoreInterfaceDefaults();
+  componentDidMount = () => {
+    window.addEventListener('resize', this.handleResize);
+  };
+
+  handleResize = (e) => {
+    let w = e.target.innerWidth;
+
+    if (w < breakpoint && this.state.layout === 'desktop') {
+      this.setState({ ...this.state, layout: 'mobile' });
+    } else if (w >= breakpoint && this.state.layout === 'mobile') {
+      this.setState({ ...this.state, layout: 'desktop' });
     }
+  };
 
-    clearState = () => {
-        this.setState({
-            username: '',
-            password: ''
-        })
-    }
+  // Controls login dialog text fields to allow reset from logout button
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    render(){
-        const { classes, history, user, showHelp, cart, userIsGuest } = this.props;
-        const { pathname } = history.location;
+  handleLogOut = () => {
+    this.clearState();
+    this.props.logOut();
+    this.props.restoreInterfaceDefaults();
+  };
 
-        const showUserDropdownMenu = Boolean(user);
-        const showLogin = Boolean(!user);
-        const showRegister = Boolean(!user);
-        const showGuest = Boolean(!user && userIsGuest && history.location.pathname.includes('visualization'));
+  clearState = () => {
+    this.setState({
+      username: '',
+      password: '',
+    });
+  };
 
-        let cartSize = cart && Object.keys(cart).length ? Object.keys(cart).length : 0;
+  render() {
+    const { classes, history, user, showHelp, cart, userIsGuest } = this.props;
+    const { pathname } = history.location;
 
-        return (
-            <div className={`${classes.navWrapper} ${!pathname.includes('/visualization') && classes.navWrapperBlue}`}>
-                {
-                    this.state.layout === 'desktop' ?
+    const showUserDropdownMenu = Boolean(user);
+    const showLogin = Boolean(!user);
+    const showRegister = Boolean(!user);
+    const showGuest = Boolean(
+      !user &&
+        userIsGuest &&
+        history.location.pathname.includes('visualization'),
+    );
 
-                    <React.Fragment>
-                        {/* Left side of navbar */}
-                        <div>
-                            <Typography variant='caption' href='/' component='a' className={`${classes.navLink} ${classes.simonsLogoWrapper}`}>
-                                <img src='/images/CMAP_white_logo_2.png' width='40' alt='CMAP logo' className={classes.simonsLogo}/>
-                            </Typography>
-                            <Typography variant='caption' to='/catalog' component={Link} className={classes.navLink}>Catalog</Typography>
-                            <VizNavbarDropdown/>
-                            {/* <Typography variant='caption' to='/visualization' component={Link} className={classes.navLink}>Visualization</Typography> */}
-                            <Typography variant='caption' to='/community' component={Link} className={classes.navLink}>Community</Typography>
-                            <DataSubmissionNavbarDropdown/>
-                            <Typography variant='caption' href='/about' component='a' className={classes.navLink}>About</Typography>
-                        </div>
+    let cartSize =
+      cart && Object.keys(cart).length ? Object.keys(cart).length : 0;
 
-                        {/* Right side of navbar */}
-                        <div className={classes.rightSectionWrapper}>
-                            <div>
-                                {
-                                    cartSize > 0 ?
-                                    <Typography variant='caption' onClick={() => this.props.setShowCart(true)} className={`${classes.navLink} ${classes.rightNavLink}`}>Favorites ({cartSize})</Typography> :
-                                    ''
-                                }
+    return (
+      <div
+        className={`${classes.navWrapper} ${
+          !pathname.includes('/visualization') && classes.navWrapperBlue
+        }`}
+      >
+        {this.state.layout === 'desktop' ? (
+          <React.Fragment>
+            {/* Left side of navbar */}
+            <div>
+              <Typography
+                variant="caption"
+                href="/"
+                component="a"
+                className={`${classes.navLink} ${classes.simonsLogoWrapper}`}
+              >
+                <img
+                  src="/images/CMAP_white_logo_2.png"
+                  width="40"
+                  alt="CMAP logo"
+                  className={classes.simonsLogo}
+                />
+              </Typography>
+              <Typography
+                variant="caption"
+                to="/catalog"
+                component={Link}
+                className={classes.navLink}
+              >
+                Catalog
+              </Typography>
+              <VizNavbarDropdown />
+              {/* <Typography variant='caption' to='/visualization' component={Link} className={classes.navLink}>Visualization</Typography> */}
+              <Typography
+                variant="caption"
+                to="/community"
+                component={Link}
+                className={classes.navLink}
+              >
+                Community
+              </Typography>
+              <DataSubmissionNavbarDropdown />
+              <Typography
+                variant="caption"
+                href="/about"
+                component="a"
+                className={classes.navLink}
+              >
+                About
+              </Typography>
+            </div>
 
-                                {showUserDropdownMenu ? <UserNavbarDropdown pathname={pathname} user={user}/> : ''}
-                                {showLogin ? <Typography variant='caption' onClick={() => this.props.showLoginDialog()} className={`${classes.navLink} ${classes.rightNavLink}`}>Log In</Typography> : ''}
-                                {showRegister ? <Typography variant='caption' to='/register' component={Link} className={`${classes.navLink} ${classes.rightNavLink}`}>Register</Typography> : ''}
-                                {showGuest ? <Typography variant='caption' onClick={() => this.props.guestPlotLimitNotificationSetIsVisible(true)} className={`${classes.navLink} ${classes.rightNavLink}`}>Guest</Typography> : ''}
-                            </div>
-                        </div>
-                    </React.Fragment>
+            {/* Right side of navbar */}
+            <div className={classes.rightSectionWrapper}>
+              <div>
+                {cartSize > 0 ? (
+                  <Typography
+                    variant="caption"
+                    onClick={() => this.props.setShowCart(true)}
+                    className={`${classes.navLink} ${classes.rightNavLink}`}
+                  >
+                    Favorites ({cartSize})
+                  </Typography>
+                ) : (
+                  ''
+                )}
 
-                    :
-
-                    <React.Fragment>
-                        <Typography variant='caption' href='/' component='a' className={`${classes.navLink} ${classes.simonsLogoWrapper}`}>
-                            <img src='/images/CMAP_white_logo_2.png' width='40' alt='CMAP logo' className={classes.simonsLogo}/>
-                        </Typography>
-                        <MobileNavbarMenu 
-                            handleLogOut={this.handleLogOut} 
-                            showLoginDialog={this.props.showLoginDialog} 
-                            user={user}
-                            cartSize={cartSize}
-                            handleShowCart={() => this.props.setShowCart(true)}
-                        />
-                    </React.Fragment>
-                }
-
-            </div>        
-        )
-    }
+                {showUserDropdownMenu ? (
+                  <UserNavbarDropdown pathname={pathname} user={user} />
+                ) : (
+                  ''
+                )}
+                {showLogin ? (
+                  <Typography
+                    variant="caption"
+                    onClick={() => this.props.showLoginDialog()}
+                    className={`${classes.navLink} ${classes.rightNavLink}`}
+                  >
+                    Log In
+                  </Typography>
+                ) : (
+                  ''
+                )}
+                {showRegister ? (
+                  <Typography
+                    variant="caption"
+                    to="/register"
+                    component={Link}
+                    className={`${classes.navLink} ${classes.rightNavLink}`}
+                  >
+                    Register
+                  </Typography>
+                ) : (
+                  ''
+                )}
+                {showGuest ? (
+                  <Typography
+                    variant="caption"
+                    onClick={() =>
+                      this.props.guestPlotLimitNotificationSetIsVisible(true)
+                    }
+                    className={`${classes.navLink} ${classes.rightNavLink}`}
+                  >
+                    Guest
+                  </Typography>
+                ) : (
+                  ''
+                )}
+              </div>
+            </div>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Typography
+              variant="caption"
+              href="/"
+              component="a"
+              className={`${classes.navLink} ${classes.simonsLogoWrapper}`}
+            >
+              <img
+                src="/images/CMAP_white_logo_2.png"
+                width="40"
+                alt="CMAP logo"
+                className={classes.simonsLogo}
+              />
+            </Typography>
+            <MobileNavbarMenu
+              handleLogOut={this.handleLogOut}
+              showLoginDialog={this.props.showLoginDialog}
+              user={user}
+              cartSize={cartSize}
+              handleShowCart={() => this.props.setShowCart(true)}
+            />
+          </React.Fragment>
+        )}
+      </div>
+    );
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(withStyles(styles)(TopNavBar)));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withRouter(withStyles(styles)(TopNavBar)));
