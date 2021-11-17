cde7728b0248e725b78fcbf2977e4de3f0a67f37
Walker Malling <mallinwa@uw.edu>
2021-11-17 13:11:51 -0800

    apply prettier to component before revising

--- src/Components/Catalog/CartAddOrRemove.js
+++ src/Components/Catalog/CartAddOrRemove.js
@@ -2,60 +2,82 @@
 
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from "react-router";
+import { withRouter } from 'react-router';
 import { withStyles, Button, Tooltip } from '@material-ui/core';
 import { Star, StarBorder } from '@material-ui/icons';
 
 import { cartAddItem, cartRemoveItem } from '../../Redux/actions/catalog';
-import { cartPersistAddItem, cartPersistRemoveItem } from '../../Redux/actions/user';
+import {
+  cartPersistAddItem,
+  cartPersistRemoveItem,
+} from '../../Redux/actions/user';
 
 const mapStateToProps = (state, ownProps) => ({
-    cart: state.cart
-})
-
-const mapDispatchToProps = {
- cartAddItem, 
- cartRemoveItem,
- cartPersistAddItem,
- cartPersistRemoveItem
-}
-
-const styles = (theme) => ({
-
+  cart: state.cart,
 });
 
+const mapDispatchToProps = {
+  cartAddItem,
+  cartRemoveItem,
+  cartPersistAddItem,
+  cartPersistRemoveItem,
+};
+
+const styles = (theme) => ({});
+
 const CartAddOrRemove = (props) => {
-    const { classes, cart, cartAddItem, cartRemoveItem, dataset, cartPersistAddItem, cartPersistRemoveItem } = props;
+  const {
+    classes,
+    cart,
+    cartAddItem,
+    cartRemoveItem,
+    dataset,
+    cartPersistAddItem,
+    cartPersistRemoveItem,
+  } = props;
 
-    const handleClick = (e) => {
-        if(cart[dataset.Long_Name]){
-            cartRemoveItem(dataset);
-            cartPersistRemoveItem(dataset.Dataset_ID);
-        }
-
-        else {
-            cartAddItem(dataset);
-            cartPersistAddItem(dataset.Dataset_ID);
-        }
+  const handleClick = (e) => {
+    if (cart[dataset.Long_Name]) {
+      cartRemoveItem(dataset);
+      cartPersistRemoveItem(dataset.Dataset_ID);
+    } else {
+      cartAddItem(dataset);
+      cartPersistAddItem(dataset.Dataset_ID);
     }
+  };
 
-    return (
-        <React.Fragment>
-            <Tooltip title={dataset.Visualize ? 'Favorites will appear first on the visualization page' : 'This dataset contains no visualizable variables, and will not appear on the visualization page.'} placement='right'>
-                <Button
-                    style={!dataset.Visualize && !cart[dataset.Long_Name] ? {color: '#e3e61a'} : {}}
-                    variant="text"
-                    color="primary"
-                    className={props.cartButtonClass}
-                    startIcon={cart[dataset.Long_Name] ? <Star/> : <StarBorder/>}
-                    onClick={handleClick}
-                >
-                    {cart[dataset.Long_Name] ? 'Remove From Favorites' : 'Add To Favorites'}
-                </Button>
-            </Tooltip>
+  return (
+    <React.Fragment>
+      <Tooltip
+        title={
+          dataset.Visualize
+            ? 'Favorites will appear first on the visualization page'
+            : 'This dataset contains no visualizable variables, and will not appear on the visualization page.'
+        }
+        placement="right"
+      >
+        <Button
+          style={
+            !dataset.Visualize && !cart[dataset.Long_Name]
+              ? { color: '#e3e61a' }
+              : {}
+          }
+          variant="text"
+          color="primary"
+          className={props.cartButtonClass}
+          startIcon={cart[dataset.Long_Name] ? <Star /> : <StarBorder />}
+          onClick={handleClick}
+        >
+          {cart[dataset.Long_Name]
+            ? 'Remove From Favorites'
+            : 'Add To Favorites'}
+        </Button>
+      </Tooltip>
+    </React.Fragment>
+  );
+};
 
-        </React.Fragment>
-    )
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(withRouter(CartAddOrRemove)));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withStyles(styles)(withRouter(CartAddOrRemove)));
