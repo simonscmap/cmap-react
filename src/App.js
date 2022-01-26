5723c5eeb9d48e069c235e2eec5423d295548c94
Walker Malling <mallinwa@uw.edu>
2022-01-26 10:46:18 -0800

    WIP add documentation route with accordian scaffold

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './Stylesheets/App.scss';
 import './Stylesheets/intro-custom.css';
 import colors from './enums/colors';
 import z from './enums/zIndex';
-
+import Docs from './Documentation';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { debounce } from 'throttle-debounce';
 
@@ -344,6 +344,10 @@ class App extends Component {
                     <Route exact path="/sample">
                       <Probe />
                     </Route>
+                     <Route exact path="/documentation">
+                      <Docs/>
+                    </Route>
+
                   </Switch>
                 </Suspense>
               </BrowserRouter>
