16d3a06c52158e171a4583b2e04c7613cbb7345d
Walker Malling <mallinwa@uw.edu>
2022-01-26 21:51:06 -0800

    wip

--- src/Documentation/sidebar.js
+++ src/Documentation/sidebar.js
@@ -4,9 +4,16 @@ import ResizeObserver from 'react-resize-observer';
 import Drawer from '@material-ui/core/Drawer';
 import docLinks from './doc-links';
 import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Fab from '@material-ui/core/Fab';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
 
 let drawerWidth = 240;
 
@@ -16,7 +23,7 @@ let styles = (theme) => ({
     color: 'white',
   },
   link: {
-    display: 'inline-block',
+    fontSize: '1.5em',
   },
   bookmarkOpen: {
     position: 'absolute',
@@ -41,13 +48,92 @@ let styles = (theme) => ({
   drawerHeader: {
     display: 'flex',
     margin: '180px 0 0 0',
-    padding: '1em',
     alignItems: 'start',
     justifyContent: 'flex-start',
     flexDirection: 'column',
   },
+  treeRoot: {
+    height: 240,
+    flexGrow: 1,
+    maxWidth: 400,
+  },
+  treeRoot: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    },
+    '&$expanded': {
+      margin: 'auto',
+    },
+  },
 });
 
+const Accordion = withStyles({
+  root: {
+    border: '0 1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    },
+    '&$expanded': {
+      // margin: 'auto',
+    },
+  },
+  expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: 'rgba(0, 0, 0, .03)',
+    borderBottom: '1px solid rgba(0, 0, 0, .125)',
+    marginBottom: -1,
+    width: '240px',
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56,
+    },
+  },
+  content: {
+    '&$expanded': {
+      // margin: '12px 0',
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiAccordionDetails);
+
+const data = {
+  id: 'root',
+  name: 'Parent',
+  children: [
+    {
+      id: '1',
+      name: 'Child - 1',
+    },
+    {
+      id: '3',
+      name: 'Child - 3',
+      children: [
+        {
+          id: '4',
+          name: 'Child - 4',
+        },
+      ],
+    },
+  ],
+};
 const Docs = (props) => {
   let { classes } = props;
   let [innerAccordionWidth, setInnerAccordionWidth] = useState(
@@ -64,27 +150,39 @@ const Docs = (props) => {
 
   let handleToggleDrawer = () => {
     setOpen(!open);
-  }
+  };
 
   let handleDrawerOpen = () => {
     setOpen(true);
   };
+
   let handleDrawerClose = () => {
     setOpen(false);
   };
+
   let setPackage = (target) => {
     return () => {
       setPkgTarget(target);
-      handleDrawerClose();
     };
   };
+
+  let accordionChange = () => {};
+
+  const renderTree = (nodes) => (
+    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </TreeItem>
+  );
+
   return (
     <div className={classes.docsWrapper}>
       <ResizeObserver onResize={onResize}></ResizeObserver>
 
-      <div className={ classes.bookmarkOpen}>
-    <Fab color="primary" onClick={handleToggleDrawer}>
-      { open ? <ChevronRightIcon />: <ChevronLeftIcon />}
+      <div className={classes.bookmarkOpen}>
+        <Fab color="primary" onClick={handleToggleDrawer}>
+          {open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
         </Fab>
       </div>
 
@@ -96,29 +194,80 @@ const Docs = (props) => {
         classes={{ paper: classes.drawerPaper }}
       >
         <div className={classes.drawerHeader}>
-          <br />
-          <Link href="#" onClick={setPackage('py')} className={classes.link}>
-            Python
-          </Link>
-
-          <br />
-          <Link href="#" onClick={setPackage('r')} className={classes.link}>
-            R
-          </Link>
-
-          <br />
-          <Link href="#" onClick={setPackage('julia')} className={classes.link}>
-            Julia
-          </Link>
-
-          <br />
-          <Link
-            href="#"
-            onClick={setPackage('matlab')}
-            className={classes.link}
+          <Accordion
+            square
+            expanded={pkgTarget === 'py'}
+            onChange={accordionChange}
           >
-            Matlab
-          </Link>
+            <AccordionSummary onClick={setPackage('py')} aria-controls="" id="">
+              <Link href="#" className={classes.link}>
+                Python
+              </Link>
+            </AccordionSummary>
+            <AccordionDetails>
+              <TreeView
+                className={classes.root}
+                defaultCollapseIcon={<ExpandMoreIcon />}
+                defaultExpanded={['root']}
+                defaultExpandIcon={<ChevronRightIcon />}
+              >
+                {renderTree(data)}
+              </TreeView>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            square
+            expanded={pkgTarget === 'r'}
+            onChange={accordionChange}
+          >
+            <AccordionSummary onClick={setPackage('r')} aria-controls="" id="">
+              <Link href="#" onClick={setPackage('r')} className={classes.link}>
+                R
+              </Link>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>R tree</Typography>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            square
+            expanded={pkgTarget === 'julia'}
+            onChange={accordionChange}
+          >
+            <AccordionSummary
+              onClick={setPackage('julia')}
+              aria-controls=""
+              id=""
+            >
+              <Link href="#" className={classes.link}>
+                Julia
+              </Link>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>Julia tree</Typography>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            square
+            expanded={pkgTarget === 'matlab'}
+            onChange={accordionChange}
+          >
+            <AccordionSummary
+              onClick={setPackage('matlab')}
+              aria-controls=""
+              id=""
+            >
+              <Link href="#" className={classes.link}>
+                Matlab
+              </Link>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>Matlab tree</Typography>
+            </AccordionDetails>
+          </Accordion>
         </div>
       </Drawer>
       <iframe
