22e77bd9c847fee66982c89b03c9965ee61a05b0
Howard Kim <hwk@uw.edu>
2025-05-23 13:03:33 -0700

    revert

--- src/Components/Catalog/DatasetCard/ContentComponents/SpatialCoverage.js
+++ src/Components/Catalog/DatasetCard/ContentComponents/SpatialCoverage.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import TableRowTextPair from './TableRowPair';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles(() => ({
+  grid: {
+    display: 'inline-grid',
+    gridTemplateColumns: 'auto auto auto',
+    '& > div': {
+      alignSelf: 'center',
+      placeSelf: 'center',
+    },
+    lineHeight: '1.1em',
+  },
+  compass: {
+    padding: '.3em',
+    '& img': {
+      width: '60px',
+    },
+  },
+  label: {
+    color: 'rgb(135, 255, 244)',
+    fontFamily: 'mono',
+  },
+}));
 
 const SpatialCoverage = (props) => {
+  const cl = useStyles();
   const { dataset } = props;
   if (!dataset) {
     return '';
@@ -13,22 +35,21 @@ const SpatialCoverage = (props) => {
   const lonmin = Lon_Min.toFixed(2);
   const lonmax = Lon_Max.toFixed(2);
 
-  const latitudeRange = `${latmin}°S – ${latmax}°N`;
-  const longitudeRange = `${lonmin}°W – ${lonmax}°E`;
-
   return (
-    <>
-      <TableRowTextPair
-        label="Latitude Range"
-        value={latitudeRange}
-        mono={true}
-      />
-      <TableRowTextPair
-        label="Longitude Range"
-        value={longitudeRange}
-        mono={true}
-      />
-    </>
+    <div className={cl.grid}>
+      <div></div>
+      <div className={cl.label}>{latmax}&deg;</div>
+      <div></div>
+
+      <div className={cl.label}>{lonmin}&deg;</div>
+      <div className={cl.compass}>
+        <img src="/images/compass.svg" width="50px" />
+      </div>
+      <div className={cl.label}>{lonmax}&deg;</div>
+      <div></div>
+      <div className={cl.label}>{latmin}&deg;</div>
+      <div></div>
+    </div>
   );
 };
 
