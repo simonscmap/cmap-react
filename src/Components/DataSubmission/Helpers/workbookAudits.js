f2bdc127164ad8a3f013a396f31e56fd3275b334
Howard Kim <hwk@uw.edu>
2025-06-03 11:54:40 -0700

    DELETE THIS COMMIT - checkNameResult

--- src/Components/DataSubmission/Helpers/workbookAudits.js
+++ src/Components/DataSubmission/Helpers/workbookAudits.js
@@ -3,9 +3,9 @@ import mainAuditExecution from './WorkbookAudits/index';
 export default (args) => {
   const { checkNameResult } = args;
   console.log('🐛🐛🐛 workbookAudits.js:5 args:', args);
-  if (!checkNameResult) {
-    return { errors: [], warnings: [], first: [], confirmations: [] };
-  }
+  // if (!checkNameResult) {
+  //   return { errors: [], warnings: [], first: [], confirmations: [] };
+  // }
 
   const results = mainAuditExecution(args);
   return results;
