7a4fd8b6158339394b1d94faaaaf7ab41fe0ccba
Howard Kim <hwk@uw.edu>
2025-07-08 14:15:29 -0700

    replace react-router with react-router-dom

--- src/Components/Catalog/CatalogSearch.js
+++ src/Components/Catalog/CatalogSearch.js
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
+import { withRouter } from 'react-router-dom';
 import { withStyles } from '@material-ui/core';
 import {
   Chip,
--- src/Components/User/ChoosePassword.js
+++ src/Components/User/ChoosePassword.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
+import { withRouter } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 import validate from './validatePassword';
 
