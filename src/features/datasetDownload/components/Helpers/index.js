412e63bb299abde4cbbbea4be004e01fc635d07d
Howard Kim <hwk@uw.edu>
2025-08-14 12:27:46 -0700

    3.1 complete

---
+++ src/features/datasetDownload/components/Helpers/index.js
@@ -0,0 +1,7 @@
+// Helper component exports
+// Files will be moved here: ValidationIndicatorBar.js, EmbeddedLogin.js, ErrorMessage.js, AncillaryDataDownload.js
+
+export { default as ValidationIndicatorBar } from './ValidationIndicatorBar';
+export { default as EmbeddedLogin } from './EmbeddedLogin';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as AncillaryDataDownload } from './AncillaryDataDownload';
---
+++ src/features/datasetDownload/components/SubsetControls/index.js
@@ -0,0 +1,8 @@
+// SubsetControls component exports
+// Files will be moved here: SubsetControls.js, DateSubsetControl.js, DepthSubsetControl.js, LatitudeSubsetControl.js, LongitudeSubsetControl.js
+
+export { default as SubsetControls } from './SubsetControls';
+export { default as DateSubsetControl } from './DateSubsetControl';
+export { default as DepthSubsetControl } from './DepthSubsetControl';
+export { default as LatitudeSubsetControl } from './LatitudeSubsetControl';
+export { default as LongitudeSubsetControl } from './LongitudeSubsetControl';
