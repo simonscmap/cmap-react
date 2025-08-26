adbf85b213316faaacc3dbc2ce35c81719a6486b
Howard Kim <hwk@uw.edu>
2025-08-26 14:59:07 -0700

    1.1 T1

---
+++ src/features/multiDatasetDownload/stores/multiDatasetDownloadStore.js
@@ -0,0 +1,71 @@
+import { create } from 'zustand';
+
+const useMultiDatasetDownloadStore = create((set, get) => ({
+  // State
+  selectedDatasets: new Set(),
+  datasets: [],
+  filters: {
+    temporal: null,
+    spatial: null,
+    depth: null,
+  },
+  isDownloading: false,
+
+  // Actions
+  toggleDatasetSelection: (datasetName) => {
+    const { selectedDatasets } = get();
+    const newSelectedDatasets = new Set(selectedDatasets);
+
+    if (newSelectedDatasets.has(datasetName)) {
+      newSelectedDatasets.delete(datasetName);
+    } else {
+      newSelectedDatasets.add(datasetName);
+    }
+
+    set({ selectedDatasets: newSelectedDatasets });
+  },
+
+  selectAll: () => {
+    const { datasets } = get();
+    const allDatasetNames = new Set(
+      datasets.map((dataset) => dataset.Dataset_Name),
+    );
+    set({ selectedDatasets: allDatasetNames });
+  },
+
+  clearSelections: () => {
+    set({ selectedDatasets: new Set() });
+  },
+
+  setDatasets: (datasets) => {
+    set({ datasets });
+  },
+
+  setFilters: (filters) => {
+    set({ filters });
+  },
+
+  setIsDownloading: (isDownloading) => {
+    set({ isDownloading });
+  },
+
+  // Computed getters
+  getSelectedDatasetCount: () => {
+    const { selectedDatasets } = get();
+    return selectedDatasets.size;
+  },
+
+  isDatasetSelected: (datasetName) => {
+    const { selectedDatasets } = get();
+    return selectedDatasets.has(datasetName);
+  },
+
+  getSelectedDatasets: () => {
+    const { selectedDatasets, datasets } = get();
+    return datasets.filter((dataset) =>
+      selectedDatasets.has(dataset.Dataset_Name),
+    );
+  },
+}));
+
+export default useMultiDatasetDownloadStore;
