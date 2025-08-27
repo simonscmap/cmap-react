43ae07066fd969d49f7ee5e7a2c3739f5759a255
Howard Kim <hwk@uw.edu>
2025-08-27 14:04:49 -0700

    update bridge

--- src/features/multiDatasetDownload/hooks/useFilteringBridge.js
+++ src/features/multiDatasetDownload/hooks/useFilteringBridge.js
@@ -13,57 +13,57 @@ const useFilteringBridge = (subsetFiltering) => {
 
   // Transform and sync filters whenever subsetFiltering state changes
   useEffect(() => {
-    if (!subsetFiltering?.subsetParams) {
+    if (!subsetFiltering?.filterValues) {
       return;
     }
 
-    const { subsetParams } = subsetFiltering;
+    const { filterValues } = subsetFiltering;
 
     // Transform temporal filters from days to date strings for API
     const temporal =
-      subsetParams.subsetIsDefined &&
-      subsetParams.Time_Min &&
-      subsetParams.timeStart !== undefined &&
-      subsetParams.timeEnd !== undefined
+      filterValues.isFiltered &&
+      filterValues.Time_Min &&
+      filterValues.timeStart !== undefined &&
+      filterValues.timeEnd !== undefined
         ? {
             startDate: dayToDateString(
-              subsetParams.Time_Min,
-              subsetParams.timeStart,
+              filterValues.Time_Min,
+              filterValues.timeStart,
             ),
             endDate: dayToDateString(
-              subsetParams.Time_Min,
-              subsetParams.timeEnd,
+              filterValues.Time_Min,
+              filterValues.timeEnd,
             ),
-            timeMin: subsetParams.Time_Min,
-            timeMax: subsetParams.Time_Max,
-            timeStart: subsetParams.timeStart,
-            timeEnd: subsetParams.timeEnd,
+            timeMin: filterValues.Time_Min,
+            timeMax: subsetFiltering.datasetFilterBounds.timeMax,
+            timeStart: filterValues.timeStart,
+            timeEnd: filterValues.timeEnd,
           }
         : null;
 
     // Transform spatial filters (lat/lon are already in correct format)
     const spatial =
-      subsetParams.subsetIsDefined &&
-      (subsetParams.latStart !== undefined ||
-        subsetParams.latEnd !== undefined ||
-        subsetParams.lonStart !== undefined ||
-        subsetParams.lonEnd !== undefined)
+      filterValues.isFiltered &&
+      (filterValues.latStart !== undefined ||
+        filterValues.latEnd !== undefined ||
+        filterValues.lonStart !== undefined ||
+        filterValues.lonEnd !== undefined)
         ? {
-            latStart: subsetParams.latStart,
-            latEnd: subsetParams.latEnd,
-            lonStart: subsetParams.lonStart,
-            lonEnd: subsetParams.lonEnd,
+            latStart: filterValues.latStart,
+            latEnd: filterValues.latEnd,
+            lonStart: filterValues.lonStart,
+            lonEnd: filterValues.lonEnd,
           }
         : null;
 
     // Transform depth filters (already in correct format)
     const depth =
-      subsetParams.subsetIsDefined &&
-      (subsetParams.depthStart !== undefined ||
-        subsetParams.depthEnd !== undefined)
+      filterValues.isFiltered &&
+      (filterValues.depthStart !== undefined ||
+        filterValues.depthEnd !== undefined)
         ? {
-            depthStart: subsetParams.depthStart,
-            depthEnd: subsetParams.depthEnd,
+            depthStart: filterValues.depthStart,
+            depthEnd: filterValues.depthEnd,
           }
         : null;
 
@@ -74,24 +74,24 @@ const useFilteringBridge = (subsetFiltering) => {
       depth,
     });
   }, [
-    subsetFiltering?.subsetParams?.subsetIsDefined,
-    subsetFiltering?.subsetParams?.Time_Min,
-    subsetFiltering?.subsetParams?.Time_Max,
-    subsetFiltering?.subsetParams?.timeStart,
-    subsetFiltering?.subsetParams?.timeEnd,
-    subsetFiltering?.subsetParams?.latStart,
-    subsetFiltering?.subsetParams?.latEnd,
-    subsetFiltering?.subsetParams?.lonStart,
-    subsetFiltering?.subsetParams?.lonEnd,
-    subsetFiltering?.subsetParams?.depthStart,
-    subsetFiltering?.subsetParams?.depthEnd,
+    subsetFiltering?.filterValues?.isFiltered,
+    subsetFiltering?.filterValues?.Time_Min,
+    subsetFiltering?.datasetFilterBounds?.timeMax,
+    subsetFiltering?.filterValues?.timeStart,
+    subsetFiltering?.filterValues?.timeEnd,
+    subsetFiltering?.filterValues?.latStart,
+    subsetFiltering?.filterValues?.latEnd,
+    subsetFiltering?.filterValues?.lonStart,
+    subsetFiltering?.filterValues?.lonEnd,
+    subsetFiltering?.filterValues?.depthStart,
+    subsetFiltering?.filterValues?.depthEnd,
     setFilters,
   ]);
 
   // Return current filter state for debugging/monitoring purposes
   return {
-    isConnected: !!subsetFiltering?.subsetParams,
-    hasFilters: subsetFiltering?.subsetParams?.subsetIsDefined || false,
+    isConnected: !!subsetFiltering?.filterValues,
+    hasFilters: subsetFiltering?.filterValues?.isFiltered || false,
   };
 };
 
