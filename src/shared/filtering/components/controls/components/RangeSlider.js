fdc1e7397f24f720a63560dfdf11c5f45cd7da93
Howard Kim <hwk@uw.edu>
2025-09-09 10:29:06 -0700

    P1

---
+++ src/shared/filtering/components/controls/components/RangeSlider.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Slider } from '@material-ui/core';
+import styles from '../../../styles/subsetControlStyles';
+
+const RangeSlider = ({
+  min,
+  max,
+  start,
+  end,
+  handleSlider,
+  step = 0.1,
+  disabled = false,
+  unit = '',
+}) => {
+  return (
+    <Slider
+      id="rangeSlider"
+      min={min}
+      max={max}
+      step={step}
+      value={[
+        typeof start === 'number' ? start : min,
+        typeof end === 'number' ? end : max,
+      ]}
+      onChange={handleSlider}
+      classes={{
+        valueLabel: styles.sliderValueLabel,
+        thumb: styles.sliderThumb,
+        markLabel: styles.markLabel,
+      }}
+      style={styles.slider}
+      disabled={disabled}
+      marks={[
+        {
+          value: min,
+          label: `${min}${unit}`,
+        },
+        {
+          value: max,
+          label: `${max}${unit}`,
+        },
+      ]}
+    />
+  );
+};
+
+export default RangeSlider;
