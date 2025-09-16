b4ae78f4c2066eaeb4a3f176f3985fb0a675662b
Howard Kim <hwk@uw.edu>
2025-09-16 15:44:05 -0700

    snackbar upgrades

--- src/Components/UI/SnackbarWrapper.js
+++ src/Components/UI/SnackbarWrapper.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 import { withStyles } from '@material-ui/core/styles';
+import InfoIcon from '@material-ui/icons/Info';
+import WarningIcon from '@material-ui/icons/Warning';
+import ErrorIcon from '@material-ui/icons/Error';
 
 import { snackbarClose } from '../../Redux/actions/ui';
 
@@ -11,10 +15,27 @@ import z from '../../enums/zIndex';
 const mapStateToProps = (state, ownProps) => ({
   snackbarIsOpen: state.snackbarIsOpen,
   snackbarMessage: state.snackbarMessage,
+  snackbarPosition: state.snackbarPosition || 'top', // backwards compatible default
+  snackbarSeverity: state.snackbarSeverity || 'info', // backwards compatible default
 });
 
 const mapDispatchToProps = {
   snackbarClose,
+};
+
+const severityConfig = {
+  info: {
+    backgroundColor: '#2196f3',
+    icon: InfoIcon,
+  },
+  warning: {
+    backgroundColor: '#ff9800',
+    icon: WarningIcon,
+  },
+  error: {
+    backgroundColor: '#f44336',
+    icon: ErrorIcon,
+  },
 };
 
 const styles = (theme) => {
@@ -22,21 +43,42 @@ const styles = (theme) => {
     snackbar: {
       zIndex: z.SNACKBAR,
     },
+    snackbarContent: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    icon: {
+      marginRight: theme.spacing(1),
+    },
   };
 };
 
 const SnackbarWrapper = (props) => {
-  const { classes } = props;
+  const { classes, snackbarPosition, snackbarSeverity } = props;
+  const config = severityConfig[snackbarSeverity];
+  const Icon = config.icon;
+
+  const vertical = snackbarPosition === 'bottom' ? 'bottom' : 'top';
+
   return (
     <React.Fragment>
       <Snackbar
         autoHideDuration={null}
-        message={props.snackbarMessage}
         onClose={props.snackbarClose}
         open={props.snackbarIsOpen}
-        anchorOrigin={{ horizontal: 'center', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'center', vertical }}
         className={classes.snackbar}
-      />
+      >
+        <SnackbarContent
+          style={{ backgroundColor: config.backgroundColor }}
+          message={
+            <span className={classes.snackbarContent}>
+              <Icon className={classes.icon} />
+              {props.snackbarMessage}
+            </span>
+          }
+        />
+      </Snackbar>
     </React.Fragment>
   );
 };
