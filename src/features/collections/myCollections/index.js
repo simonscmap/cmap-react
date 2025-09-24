283475a782870600289e652cffc91f18a8aebbbd
Howard Kim <hwk@uw.edu>
2025-09-24 13:52:38 -0700

    T008

--- src/features/collections/myCollections/index.js
+++ src/features/collections/myCollections/index.js
@@ -1,7 +1,6 @@
 // My Collections Domain - Public API
 // Components for personal collections management
 
-// To be implemented in Phase 3.2
-// export { MyCollectionsTab } from './MyCollectionsTab';
-// export { CollectionCard } from './CollectionCard';
-// export { CollectionStatistics } from './CollectionStatistics';
+export { default as MyCollectionsTab } from './MyCollectionsTab';
+export { default as CollectionCard } from './CollectionCard';
+export { default as CollectionStatistics } from './CollectionStatistics';
