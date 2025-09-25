f008d04c14a892ac4ce72566604b432610e565e0
Howard Kim <hwk@uw.edu>
2025-09-25 12:51:52 -0700

    rm safe object

--- src/api/collectionsApi.js
+++ src/api/collectionsApi.js
@@ -27,38 +27,4 @@ collectionsAPI.trackCopy = async (collectionId) => {
   return await fetch(endpoint, postOptions);
 };
 
-// Wrap each endpoint in a try/catch because...
-// if the fetch fails before the request is sent, for example due to a CORS
-// violation or if the network is down,
-// then redux-saga will choke and no subsequent middleware will run,
-// effectively crippling the application.
-// yay error handling.
-
-let safeAPI = Object.entries(collectionsAPI)
-  .map(([name, fn]) => {
-    return {
-      [name]: async (...args) => {
-        let result;
-        try {
-          result = await fn.apply(null, args);
-        } catch (e) {
-          if (e) {
-            result = e;
-          } else {
-            result = new Error(
-              'unknown error, the request may not have been sent',
-            );
-          }
-        }
-        return await result;
-      },
-    };
-  })
-  .reduce((accumulator, current) => {
-    return {
-      ...accumulator,
-      ...current,
-    };
-  }, {});
-
-export default safeAPI;
+export default collectionsAPI;
